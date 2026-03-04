a67b0c19530f6167914f04d08523e7110220e9cf
subburayalu061 <subburayalu061@gmail.com>
2026-03-04 14:44:29 +0530

    Refactor ALU control signals for clarity

--- RISC V/ALU_Decoder.v
+++ RISC V/ALU_Decoder.v
@@ -9,39 +9,39 @@ always @(*) begin
 
     case(ALUOp)
 
-        2'b00: cntl = 4'b0011; // ADD
+        2'b00: cntl = 4'b0011;
 
-        2'b01: cntl = 4'b0100; // SUB (BEQ)
+        2'b01: cntl = 4'b0100; 
 
         2'b10: begin
             case(funct3)
 
                 3'b000: begin
                     if (funct7[5])
-                        cntl = 4'b0100; // SUB
+                        cntl = 4'b0100;
                     else
-                        cntl = 4'b0011; // ADD
+                        cntl = 4'b0011;
                 end
 
-                3'b111: cntl = 4'b0000; // AND
-                3'b110: cntl = 4'b0001; // OR
-                3'b100: cntl = 4'b0010; // XOR
-                3'b010: cntl = 4'b0101; // SLT
-                3'b011: cntl = 4'b0110; // SLTU
-                3'b001: cntl = 4'b0111; // SLL
+                3'b111: cntl = 4'b0000; 
+                3'b110: cntl = 4'b0001;
+                3'b100: cntl = 4'b0010;
+                3'b010: cntl = 4'b0101;
+                3'b011: cntl = 4'b0110;
+                3'b001: cntl = 4'b0111;
 
                 3'b101: begin
                     if (funct7[5])
-                        cntl = 4'b1000; // SRA
+                        cntl = 4'b1000;
                     else
-                        cntl = 4'b1001; // SRL
+                        cntl = 4'b1001;
                 end
 
                 default: cntl = 4'b0011;
             endcase
         end
 
-        2'b11: cntl = 4'b1010; // PASS B (LUI)
+        2'b11: cntl = 4'b1010;
 
         default: cntl = 4'b0011;
 
